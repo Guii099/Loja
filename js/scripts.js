fb452ce6d65c1be046a67bfe4c3ab35e227a2bf0
Guii099 <gui20099trabalho@gmail.com>
2021-12-15 16:26:41 -0300

    Footer e JavaScript Adicionados

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+function cs() {
+    document.getElementById("fundocs").style.backgroundColor = "rgba(0, 0, 0, 0.350)"
+}
+
+function lol() {
+    document.getElementById("fundolol").style.backgroundColor = "rgba(0, 0, 0, 0.350)"
+}
+
+function vava() {
+    document.getElementById("fundovava").style.backgroundColor = "rgba(0, 0, 0, 0.350)"
+}
+
+function fort() {
+    document.getElementById("fundofort").style.backgroundColor = "rgba(0, 0, 0, 0.350)"
+}
